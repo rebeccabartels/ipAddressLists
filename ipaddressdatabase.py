4994954ad703391e023c2431f40724227c8f0f19
Rebecca Bartels <rebeccakbartels@gmail.com>
2019-10-13 16:39:45 -0400

    Create ipaddressdatabase.py

---
+++ ipaddressdatabase.py
@@ -0,0 +1,28 @@
+database = {}
+providers = ["Level3", "Verisign", "Google", "Quad9", "DNS.WATCH",
+             "Comodo Secure DNS", "OpenDNS Home", "Norton ConnectSafe",
+             "GreenTeamDNS", "SafeDNS", "OpenNIC", "SmartViper", "Dyn",
+             "FreeDNS", "Alternate DNS", "Yandex.DNS", "UncensoredDNS",
+             "Hurricane Electric", "puntCAT", "Neustar", "Cloudflare",
+             "Fourth Estate"]
+
+ips = ["209.244.0.3", "64.6.64.6", "8.8.8.8", "9.9.9.9", "84.200.69.80",
+       "8.26.56.26", "208.67.222.222", "199.85.126.10", "81.218.119.11",
+       "195.46.39.39", "69.195.152.204", "208.76.50.50", "216.146.35.35",
+       "37.235.1.174", "198.101.242.72", "77.88.8.8", "91.239.100.100",
+       "74.82.42.42", "109.69.8.51", "156.154.70.1", "1.1.1.1", "45.77.165.194"]
+
+
+# create database dictionary and match
+
+def createDatabase():
+
+    for provider in (providers):
+        database = dict(zip(providers, ips))
+
+        # Creating separation between each new item prompt
+        print(database)
+        break
+
+
+createDatabase()
