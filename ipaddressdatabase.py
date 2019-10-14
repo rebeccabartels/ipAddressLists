bf5a3049785aff03a4936d446025c566966e42d6
Rebecca Bartels <rebeccakbartels@gmail.com>
2019-10-14 15:36:30 -0400

    Update ipaddressdatabase.py
    made dict global variable so it can be accessed in other funtions

--- ipaddressdatabase.py
+++ ipaddressdatabase.py
@@ -1,34 +1,44 @@
 
-providers = ["Level3", "Verisign", "Google", "Quad9", "DNS.WATCH",
-             "Comodo Secure DNS", "OpenDNS Home", "Norton ConnectSafe",
-             "GreenTeamDNS", "SafeDNS", "OpenNIC", "SmartViper", "Dyn",
-             "FreeDNS", "Alternate DNS", "Yandex.DNS", "UncensoredDNS",
-             "Hurricane Electric", "puntCAT", "Neustar", "Cloudflare",
-             "Fourth Estate"]
-
-ips = ["209.244.0.3", "64.6.64.6", "8.8.8.8", "9.9.9.9", "84.200.69.80",
-       "8.26.56.26", "208.67.222.222", "199.85.126.10", "81.218.119.11",
-       "195.46.39.39", "69.195.152.204", "208.76.50.50", "216.146.35.35",
-       "37.235.1.174", "198.101.242.72", "77.88.8.8", "91.239.100.100",
-       "74.82.42.42", "109.69.8.51", "156.154.70.1", "1.1.1.1", "45.77.165.194"]
+DNS_dictionary = {}
 
 
-# create database dictionary and match
+def create():
 
+    providers = ["Level3", "Verisign", "Google", "Quad9", "DNS.WATCH",
+                 "Comodo Secure DNS", "OpenDNS Home", "Norton ConnectSafe",
+                 "GreenTeamDNS", "SafeDNS", "OpenNIC", "SmartViper", "Dyn",
+                 "FreeDNS", "Alternate DNS", "Yandex.DNS", "UncensoredDNS",
+                 "Hurricane Electric", "puntCAT", "Neustar", "Cloudflare",
+                 "Fourth Estate"]
 
-def createData():
+    ips = ["209.244.0.3", "64.6.64.6", "8.8.8.8", "9.9.9.9", "84.200.69.80",
+           "8.26.56.26", "208.67.222.222", "199.85.126.10", "81.218.119.11",
+           "195.46.39.39", "69.195.152.204", "208.76.50.50", "216.146.35.35",
+           "37.235.1.174", "198.101.242.72", "77.88.8.8", "91.239.100.100",
+           "74.82.42.42", "109.69.8.51", "156.154.70.1", "1.1.1.1", "45.77.165.194"]
+
+    # create database dictionary and match
+    global DNS_dictionary
 
     for provider in (providers):
         data = dict(zip(providers, ips))
-        print(data)
+        # print(data)
+
         for company in data.keys():
 
             print(company + " => " + data[company])
 
         # Creating separation between each new item prompt
-        # print(data["Level3"])
+        DNS_dictionary = data
 
         break
 
+    # print(DNS_dictionary.keys())
+    # print(DNS_dictionary.values())
+    # print(DNS_dictionary.items())
 
-createData()
+
+create()
+
+
+print(DNS_dictionary)
